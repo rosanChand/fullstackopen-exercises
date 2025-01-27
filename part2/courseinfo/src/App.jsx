f2278e6de5a191ddd870f54273bda081c21dd310
Roshan Chand <rosanchand1234@gmail.com>
2025-01-27 17:41:31 +0545

    *solution to exercise 2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,28 +1,5 @@
+import Course from "./components/Course"
 
-const Part = ({parts})=>{
-  return <p>{parts.name} {parts.exercises}</p>
-}
-
-const Content = ({parts}) =>{
-  return(
-  <>
-  {console.log('dsjkng')}
-  {parts.map(part => ( <Part key={part.id} parts={part} />))}
-  </>
-  )
-}
-
-const Header = ({name}) =>{
-  return <h1>{name}</h1>
-}
-const Course = ({course}) =>{
-  return (
-    <>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    </>
-  )
-}
 
 const App = () => {
   const course = {
