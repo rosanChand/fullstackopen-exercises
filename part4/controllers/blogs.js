3de357c6253552728f66895aa1d29e17b3da143e
Roshan Chand <rosanchand1234@gmail.com>
2025-04-26 09:31:29 +0545

    refactored post  to use async/await

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -13,17 +13,24 @@ blogsRouter.get('/', async (request, response) => {
   //   })
 })
 
-blogsRouter.post('/', (request, response,next) => {
+blogsRouter.post('/', async (request, response,next) => {
   const blog = new Blog(request.body)
   if (!blog.title || ! blog.url){
     response.status(400).end()
   }
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  try{
+  const result = await blog.save()
+  response.status(201).json(result)
+} catch (ex){
+  next(ex)
+}
+  // blog
+  //   .save()
+  //   .then(result => {
+  //     response.status(201).json(result)
+  //   })
+  //   .catch(error => next(error))
 })
+
 
 module.exports = blogsRouter
